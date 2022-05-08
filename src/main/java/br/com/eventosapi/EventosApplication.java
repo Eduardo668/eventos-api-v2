7e248ac5e1db7c769e9582f705b3c0df3d399e30
Eduardo668 <78269439+Eduardo668@users.noreply.github.com>
2022-05-08 11:42:39 -0300

    First Commit

---
+++ src/main/java/br/com/eventosapi/EventosApplication.java
@@ -0,0 +1,13 @@
+package br.com.eventosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosApplication.class, args);
+	}
+
+}
